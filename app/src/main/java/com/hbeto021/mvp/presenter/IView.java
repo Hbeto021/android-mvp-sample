f8277c2bc59c77194c2079e6776e8e0a24e94cb8
Hbeto021 <humberto.cesar01@outlook.com>
2019-05-02 16:54:09 -0300

    Creates the interface that provides methods for MainActivity

---
+++ app/src/main/java/com/hbeto021/mvp/presenter/IView.java
@@ -0,0 +1,16 @@
+package com.hbeto021.mvp.presenter;
+
+/**
+ * Interface that provides methods for MainActivity
+ * This methods updates the view and are called for Presenter
+ *
+ */
+public interface IView {
+
+    void getMessageToUser(String message);
+
+    void getEmailDetais(String emailDetails);
+
+    void resetFields();
+
+}
