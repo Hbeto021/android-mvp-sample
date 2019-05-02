2366c14410fadc0f5eff968772c706e8f115b239
Hbeto021 <humberto.cesar01@outlook.com>
2019-05-02 16:50:43 -0300

    Creating the Presenter to update and receive requests from view

---
+++ app/src/main/java/com/hbeto021/mvp/presenter/Presenter.java
@@ -0,0 +1,39 @@
+package com.hbeto021.mvp.presenter;
+
+import com.hbeto021.mvp.model.Email;
+
+import java.util.Date;
+
+
+/**
+ * The middle guy between view and model
+ * Remove any logic from view
+ * Makes all the view logic
+ * Presents usable data to the view layer and update the view
+ */
+public class Presenter implements IPresenter {
+
+    private IView view;
+
+    public Presenter(IView view) {
+        this.view = view;
+    }
+
+    @Override
+    public void sendEmail(String recipient, String subject, String message) {
+        Email email = new Email(recipient, subject, message);
+        if (email.fieldsAreComplete()) {
+            if (email.recipientIsValid()) {
+                //sets the current date
+                email.setDate(new Date(System.currentTimeMillis()));
+                view.getMessageToUser("Email sent with success.");
+                view.getEmailDetais(email.toString());
+                view.resetFields();
+            } else {
+                view.getMessageToUser("Recipient email is not a correct email, please insert a correct.");
+            }
+        } else {
+            view.getMessageToUser("There are some empty fields.");
+        }
+    }
+}
