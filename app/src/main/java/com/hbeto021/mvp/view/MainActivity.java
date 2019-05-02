c086bc90c186e80a15c874cd95a4e1405d00804d
Hbeto021 <humberto.cesar01@outlook.com>
2019-05-02 16:57:28 -0300

    Preparing the activity to receive updates from Presenter

---
+++ app/src/main/java/com/hbeto021/mvp/view/MainActivity.java
@@ -0,0 +1,69 @@
+package com.hbeto021.mvp.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.hbeto021.mvp.R;
+import com.hbeto021.mvp.model.Email;
+import com.hbeto021.mvp.presenter.IPresenter;
+import com.hbeto021.mvp.presenter.IView;
+import com.hbeto021.mvp.presenter.Presenter;
+
+public class MainActivity extends AppCompatActivity implements IView {
+
+    private EditText edtRecipient, edtSubject, edtMessage;
+    private TextView tvEmailDetails;
+
+
+    private IPresenter presenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        presenter = new Presenter(this);
+
+        initViews();
+    }
+
+
+    private void initViews() {
+        edtRecipient = findViewById(R.id.edt_recipient);
+        edtSubject = findViewById(R.id.edt_subject);
+        edtMessage = findViewById(R.id.edt_message);
+        tvEmailDetails = findViewById(R.id.tv_email_details);
+
+        Button btnSend = findViewById(R.id.btn_send);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvEmailDetails.setText("");
+                presenter.sendEmail(edtRecipient.getText().toString(), edtSubject.getText().toString(),
+                        edtMessage.getText().toString());
+            }
+        });
+    }
+
+    @Override
+    public void getMessageToUser(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void getEmailDetais(String emailDetails) {
+        tvEmailDetails.setText(emailDetails);
+    }
+
+    @Override
+    public void resetFields() {
+        edtRecipient.setText("");
+        edtSubject.setText("");
+        edtMessage.setText("");
+    }
+}
